0bb8ee9cd490ed36b2fc1692ea7f8e922763bee0
Mohammed23200 <mgsa112233@gmail.com>
2025-07-04 08:14:53 +0300

    Start with ball

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from turtle import Screen,Turtle
 from paddle import Paddle
+from ball import Ball
+import time
+
 screen = Screen()
 screen.bgcolor("black")
 screen.title("ponk")
@@ -7,6 +10,7 @@ screen.setup(height=600,width=800)
 screen.tracer(0)
 paddle_left = Paddle(position=-350)
 paddle_right = Paddle(position=350)
+ball = Ball()
 screen.listen()
 screen.onkey(key='Up',fun=paddle_right.up)
 screen.onkey(key='Down',fun=paddle_right.down)
@@ -14,5 +18,7 @@ screen.onkey(key='w',fun=paddle_left.up)
 screen.onkey(key='s',fun=paddle_left.down) 
 is_game_on = True
 while is_game_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 screen.exitonclick()
